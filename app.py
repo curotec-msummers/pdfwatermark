3db7df76d491a3a9cc3c1e6e4b103727195a3a36
curotec-msummers <115308186+curotec-msummers@users.noreply.github.com>
2023-10-17 00:12:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, request, Response
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse, abort
 import io
 import PyPDF2
 import requests
 
 app = Flask(__name__)
+api = Api(app)
+
+# Define a dictionary to store valid API keys (you should securely manage these keys)
+api_keys = {"f5762a00-1a5c-4ac2-aa97-2447492b05cf": "d0cb8e72-88c5-4c11-a074-443761fcb51a"}
+
+def validate_api_key(api_key):
+    return api_keys.get(api_key) is not None
 
 def add_watermark(pdf_bytes, watermark_text):
     pdf = PyPDF2.PdfReader(io.BytesIO(pdf_bytes))
@@ -20,36 +28,36 @@ def add_watermark(pdf_bytes, watermark_text):
 
     return output_pdf_bytes.getvalue()
 
-@app.route('/watermark-pdf', methods=['POST'])
-def watermark_pdf():
-    try:
-        # Get PDF URL and watermark text from the request
-        pdf_url = request.form.get('pdf_url')
-        watermark_text = request.form.get('watermark_text')
+class WatermarkPDFResource(Resource):
+    def post(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('api_key', type=str, required=True, help='API Key is required')
+            parser.add_argument('pdf_url', type=str, required=True, help='PDF URL is required')
+            parser.add_argument('watermark_text', type=str, required=True, help='Watermark text is required')
+            args = parser.parse_args()
 
-        # Validate inputs
-        if not pdf_url or not watermark_text:
-            return "Both 'pdf_url' and 'watermark_text' are required.", 400
+            # Validate the API key
+            if not validate_api_key(args['api_key']):
+                abort(401, message='Invalid API Key')
 
-        # Fetch the PDF file from the provided URL
-        response = requests.get(pdf_url)
-        if response.status_code != 200:
-            return "Failed to fetch the PDF from the provided URL.", 400
+            # Fetch the PDF file from the provided URL
+            response = requests.get(args['pdf_url'])
+            if response.status_code != 200:
+                abort(400, message='Failed to fetch the PDF from the provided URL')
 
-        # Create a watermark (as a PyPDF2 object)
-        watermark = PyPDF2.PdfReader(io.BytesIO(watermark_text.encode()))
+            # Create a watermark (as a PyPDF2 object)
+            watermark = PyPDF2.PdfFileReader(io.BytesIO(args['watermark_text'].encode()))
 
-        # Add watermark to the PDF
-        watermarked_pdf_bytes = add_watermark(response.content, watermark)
+            # Add watermark to the PDF
+            watermarked_pdf_bytes = add_watermark(response.content, watermark)
 
-        # Create a response with the watermarked PDF
-        response = Response(watermarked_pdf_bytes, content_type='application/pdf')
-        response.headers['Content-Disposition'] = 'inline; filename=watermarked.pdf'
+            return {'watermarked_pdf': watermarked_pdf_bytes.decode('latin1')}
 
-        return response
+        except Exception as e:
+            abort(500, message=str(e))
 
-    except Exception as e:
-        return str(e), 500  # Return an error response if anything goes wrong
+api.add_resource(WatermarkPDFResource, '/watermark-pdf')
 
 if __name__ == '__main__':
     app.run(debug=False)
